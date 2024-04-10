5be60c558c9c287c94df272548e4b601fd5fe3ec
Arefa Satellite <aivanov@up2u.one>
2024-04-10 16:54:44 +0300

    changes by ArefaSatellite:  - added TOken class   -- added string value

---
+++ app/src/main/java/com/example/api_project/Token.java
@@ -0,0 +1,32 @@
+package com.example.api_project;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Token {
+    private final SharedPreferences cache_storage;
+    public Token(AppCompatActivity activity){
+        this.cache_storage = activity.getSharedPreferences(
+                String.valueOf(R.string.cache_token_storage),
+                Context.MODE_PRIVATE
+        );
+    }
+    public String getToken(){
+        return this.cache_storage.getString("user_token", null);
+    }
+    public void setToken(String token_temp){
+        SharedPreferences.Editor editor = cache_storage.edit();
+        editor.putString("user_token", token_temp);
+        editor.apply();
+    }
+    public void removeToken(){
+        SharedPreferences.Editor editor = cache_storage.edit();
+        editor.putString("user_token", null);
+        editor.apply();
+    }
+    public boolean isEmptyToken(){
+        return this.getToken().isEmpty();
+    }
+}
